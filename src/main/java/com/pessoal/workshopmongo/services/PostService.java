44662287c7d14518597cbc33089d144c6bafbfe8
Vinícius <viniciusmoraesdasilva25@gmail.com>
2024-11-20 23:35:11 -0300

    consulta simples com query methods

--- src/main/java/com/pessoal/workshopmongo/services/PostService.java
+++ src/main/java/com/pessoal/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.pessoal.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> user = postRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
